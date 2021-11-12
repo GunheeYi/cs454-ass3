8d9f3b1fe298b91a471cecb33d1040d9ae1d0a25
Gunhee Yi <gunny2676@gmail.com>
2021-11-12 22:27:57 +0900

    pooling for any cases; random for N under 50

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from math import sqrt
-from random import shuffle, randrange, sample
+from random import randrange, sample
 import argparse
+import json
 
 class XY:
     def __init__(self, x, y):
@@ -16,8 +17,9 @@ def dist(xy1, xy2):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
-parser.add_argument('-p', required=False, default='100', help='population')
+parser.add_argument('-p', required=False, default='10', help='population')
 parser.add_argument('-f', required=False, default='100000', help='limit on number of fitness evaluations')
+parser.add_argument('-c', required=False, default='', help='previous checkpoints')
 args = parser.parse_args()
 
 pts = [XY(0,0)]
@@ -25,7 +27,7 @@ pts = [XY(0,0)]
 with open(args.filename, 'r') as file:
     while True:
         line = file.readline()
-        if line=='NODE_COORD_SECTION\n': break
+        if line in ['NODE_COORD_SECTION\n', 'DISPLAY_DATA_SECTION\n']: break
         # print(line, end="")
     while True:
         line = file.readline()
@@ -34,12 +36,16 @@ with open(args.filename, 'r') as file:
         pts += [XY(float(words[1]), float(words[2]))]
 
 N = len(pts)-1
+if N < 2: raise ValueError("Less than 2 points provided. Solver needs at least 2 points to solve the problem.")
 POP = int(args.p)
-EPOCHS = 3000
+EPOCHS = 10000
 EVAL_LIMIT = int(args.f)
 EVAL_COUNT = 0
 RATE_CROSSOVER = 1
 RATE_MUTATION = 1
+CHECKPOINT = args.c
+
+root = list(range(1, N+1)) # ordered list of points, later to be used to generate random permutations
 
 def cost(l):
     global EVAL_COUNT
@@ -90,11 +96,14 @@ def mutateAdjacent(l):
     i = randrange(n)
     return swap(l, i, 0 if i==n-1 else i+1)
 
-def optimize(l=[], pool=[], pop=20, epochs=20, rate_crossover=1, rate_mutation=1, printt=False, savee=False):
-    if not (l or pool) or (l and pool): raise ValueError # at least one of sol or pool of sol must be provided
-    if l: # if sol is given, initialize pool by random permutating
-        pool = []
-        for _ in range(pop*2): pool.append(sample(l, len(l)))
+def save(sol, pool):
+    solStr = ""
+    for i in sol: solStr += str(i)+"\n"
+    with open('sol_{}.csv'.format(cost(sol)), 'w') as solution: solution.write(solStr[:-1])
+    with open('ckpt_{}.json'.format(cost(sol)), 'w') as poolF: json.dump(pool, poolF, indent=2)
+
+def optimize(pool, pop=POP, epochs=EPOCHS, rate_crossover=1, rate_mutation=1, printt=False, savee=False):
+    global EVAL_LIMIT, EVAL_COUNT
     
     for epoch in range(epochs):
         crossovers = []
@@ -102,90 +111,82 @@ def optimize(l=[], pool=[], pop=20, epochs=20, rate_crossover=1, rate_mutation=1
             p1, p2 = sample(pool, 2)
             crossovers += [crossoverPMX(p1, p2)]
         
-        mutants = []
-        while len(mutants) < pop*rate_mutation:
+        mutantsAdjacent = []
+        while len(mutantsAdjacent) < pop*rate_mutation:
             p = sample(pool, 1)[0]
-            mutants += [mutateAdjacent(p)]
+            mutantsAdjacent += [mutateAdjacent(p)]
+        mutantsRandom = []
+        while len(mutantsRandom) < pop*rate_mutation:
+            p = sample(pool, 1)[0]
+            mutantsRandom += [mutateRandom(p)]
+        
+        poolTemp = pool + crossovers + mutantsAdjacent + mutantsRandom # merge all path pools
+        poolTemp.sort(key=lambda _: cost(_)) # select only the first POP paths fittest
 
-        # merge all path pools
-        pool = pool + crossovers + mutants
-        # select only the first POP paths fittest
-        pool.sort(key=lambda _: cost(_))
-        pool = pool[0:pop]
+        if EVAL_COUNT > EVAL_LIMIT: # save and return immediately if evaluation limit exceeded
+            print("Evaluation limit exceeded. The fittest current cycle costs {}".format(cost(pool[0])))
+            save(pool[0], pool)
+            return pool[0]
+        pool = poolTemp[0:pop]
 
-        if savee:
-            solStr = ""
-            for i in pool[0]: solStr += str(i)+"\n"
-            with open('solution - cost {}.csv'.format(cost(pool[0])), 'w') as solution: solution.write(solStr[:-1])
+        
+        if savee: save(pool[0], pool) # save the best solution in current generation
+        if printt: # print the best solution in current generation
+            print("The fittest cycle in epoch {} costs {}".format(epoch+1, cost(pool[0])))
 
-        # print the best path in current generation
-        if printt: print("The fittest cycle in epoch {} costs {}".format(epoch+1, cost(pool[0])))
-    return pool[0]
+    save(pool[0], pool)
+    return 
 
 xs = list(map(lambda _: _.x, pts))
 ys = list(map(lambda _: _.y, pts))
 xMin, xMax, yMin, yMax = min(xs), max(xs)+1, min(ys), max(ys)+1
 # (0 19153.0 0 13146.0)
 
+def init_grid(xDiv, yDiv, xDir):
+    xSpan, ySpan = (xMax-xMin)/xDiv, (yMax-yMin)/yDiv
+    ptsGrid = [[[] for _ in range(yDiv)] for _ in range(xDiv)]
 
-pool = []
+    for idx, pt in enumerate(pts[1:]):
+        i, j = int((pt.x-xMin)/xSpan), int((pt.y-yMin)/ySpan)
+        ptsGrid[i][j] += [idx+1]
 
-# initiate pool by random permutation
-# root = list(range(1,N+1))
-# for _ in range(POP*2):
-#     pool.append(sample(root, N))
-
-# initiate pool based on grid
-for xDiv in range(87, 88, 1):
-    for yDiv in range(309, 310, 1):
-        print("xDiv {}, yDiv {}".format(xDiv, yDiv))
-        xSpan, ySpan = (xMax-xMin)/xDiv, (yMax-yMin)/yDiv
-        ptsGrid = [[[] for _ in range(yDiv)] for _ in range(xDiv)]
-
-        for idx, pt in enumerate(pts[1:]):
-            i, j = int((pt.x-xMin)/xSpan), int((pt.y-yMin)/ySpan)
-            ptsGrid[i][j] += [idx+1]
-
-        sol = []
+    sol = []
+    if xDir:
         for i in range(xDiv):
             for j in (range(yDiv) if i%2==0 else reversed(range(yDiv))):
-                print(i, j)
-                if ptsGrid[i][j]:
-                    sol += optimize(l=ptsGrid[i][j])
-                    # sol += ptsGrid[i][j]
-        pool.append({
-            'xDiv': xDiv,
-            'yDiv': yDiv,
-            'dir': 'x',
-            'sol': sol
-        })
-        
-        # sol = []
-        # for i in range(yDiv):
-        #     for j in (range(xDiv) if i%2==0 else reversed(range(xDiv))):
-        #         if ptsGrid[j][i]:
-        #             sol += optimize(l=ptsGrid[j][i])
-        # pool.append({
-        #     'xDiv': xDiv,
-        #     'yDiv': yDiv,
-        #     'dir': 'y',
-        #     'sol': sol
-        # })
+                if ptsGrid[i][j]: sol += ptsGrid[i][j]
+    else:
+        for i in range(yDiv):
+            for j in (range(xDiv) if i%2==0 else reversed(range(xDiv))):
+                if ptsGrid[j][i]: sol += ptsGrid[j][i]
+    
+    return sol
 
+if CHECKPOINT:
+    with open(CHECKPOINT, 'r') as f: pool = json.load(f)
+else:
+    pool = []
 
-pool.sort(key=lambda _: cost(_['sol']))
+    if N <= 50:
+        for _ in range(POP): pool.append(sample(root, N))
+    else:
+        estSide = sqrt(N)
+        divMin, divMax = max(int(estSide*0.5), 1), max(int(estSide*1.5), 2)+1
+        divStep = max(int(estSide*0.05), 1)
+        for xDiv in range(divMin, divMax, divStep):
+            for yDiv in range(divMin, divMax, divStep):
+                pool.append(init_grid(xDiv, yDiv, True))
+                pool.append(init_grid(xDiv, yDiv, False))
+    
+    # pool.append(init_grid(87, 309, True))
+    # pool.append(init_grid(960, 61, False))
+
+pool.sort(key=cost)
 pool = pool[0:POP]
 
 print("Initial pool------------------")
-for d in pool:
-    print("xDiv {}, yDiv {}, dir {}, cost {}".format(d['xDiv'], d['yDiv'], d['dir'], cost(d['sol'])))
+for sol in pool:
+    print("cost {}".format(cost(sol)))
 print("-------------------------------")
 
-# optimize(pool=pool, pop=POP, epochs=EPOCHS, 
-#     rate_crossover=RATE_CROSSOVER, rate_mutation=RATE_MUTATION, 
-#     printt=True, savee=True
-# )
-
-solStr = ""
-for i in pool[0]['sol']: solStr += str(i)+"\n"
-with open('solution - cost {}.csv'.format(cost(pool[0]['sol'])), 'w') as solution: solution.write(solStr[:-1])
+optimize(pool, printt=True, savee=True)
